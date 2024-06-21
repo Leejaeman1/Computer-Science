cf8584aed3297dc1598b1128c2645bd45066e30f
ObedientManx700 <yeonjunjeong0@gmail.com>
2024-06-21 13:20:39 +0900

    Update fetch.sh

--- exec/exec/fetch.sh
+++ exec/exec/fetch.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #MANAGEMENT_SERVER_URL="https://jsonplaceholder.typicode.com/posts/1"
-#관리서버URL주소 (신규 제출된 SUBMITTED 상태의 py 파일 요청하기)
 MANAGEMENT_SERVER_URL="http://25.32.99.72/new"
 
 
